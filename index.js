322272dbaee848a4d155df4c7f06fd1e5b05f6a9
jwoleslagle <30732086+jwoleslagle@users.noreply.github.com>
2018-02-05 16:49:01 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+let geocoder, map;
+
+function initMap() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+        zoom: 8,
+        center: latlng
+        }
+    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+}
+
+function codeAddress() {
+    let address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+        } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+$(initMap);
