9443325fa19bf00ec79370b7820b3db0e495aaa6
jwoleslagle <30732086+jwoleslagle@users.noreply.github.com>
2018-02-09 17:52:21 -0500

    Fixing census queries

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = express()
 server.use(express.static("."))
 
 server.get("/api", (req, res) => {
+  // Example query: http://data.fcc.gov/api/block/find?format=json&latitude=41.2380564&longitude=-96.1429296&showall=true
   console.log('Server request ran.')
   const params = req.query
   const url =
@@ -28,16 +29,16 @@ server.get("/ac5_tract", (req, res) => {
   const params = req.query
 
   const url =
-    "https://api.census.gov/data/2016/acs/acs5?get=NAME,B00001_001E,B00002_001E,B20002_001E,B25012_002E&for=tract:" + params.tract + "$in=state:" + params.stateCode + "%20county:" + params.county + "&key=37ff14711f40a5b4b3f6d39f773e687047ea6ce3"
+    "https://api.census.gov/data/2016/acs/acs5?get=B00001_001E,B00002_001E,B20002_001E,B25012_002E&for=tract:" + params.tract + "$in=state:" + params.stateCode + "%20county:" + params.county + "&key=37ff14711f40a5b4b3f6d39f773e687047ea6ce3"
 
   axios
     .get(url)
     .then(apiRes => {
       let tractResult = {
-        totalPop: apiRes.data.B00001_001E,
-        totalHousingUnits: apiRes.data.B00002_001E,
-        medianIncome: apiRes.data.B20002_001E,
-        ownerOccupiedHousingUnits: apiRes.data.B25012_002E
+        totalPop: apiRes.data[1].B00001_001E,
+        totalHousingUnits: apiRes.data[1].B00002_001E,
+        medianIncome: apiRes.data[1].B20002_001E,
+        ownerOccupiedHousingUnits: apiRes.data[1].B25012_002E
       }
       res.json(tractResult)
     })
@@ -50,16 +51,16 @@ server.get("/ac5_county", (req, res) => {
   const params = req.query
 
   const url =
-    "https://api.census.gov/data/2016/acs/acs5?get=NAME,B00001_001E,B00002_001E,B20002_001E,B25012_002E&for=county:" + params.county + "$in=state:" + params.stateCode + "&key=37ff14711f40a5b4b3f6d39f773e687047ea6ce3"
+    "https://api.census.gov/data/2016/acs/acs5?get=B00001_001E,B00002_001E,B20002_001E,B25012_002E&for=county:" + params.county + "$in=state:" + params.stateCode + "&key=37ff14711f40a5b4b3f6d39f773e687047ea6ce3"
 
   axios
     .get(url)
     .then(data => {
       let countyResult = {
-        totalPop: data.B00001_001E,
-        totalHousingUnits: data.B00002_001E,
-        medianIncome: data.B20002_001E,
-        ownerOccupiedHousingUnits: data.B25012_002E
+        totalPop: data[1].B00001_001E,
+        totalHousingUnits: data[1].B00002_001E,
+        medianIncome: data[1].B20002_001E,
+        ownerOccupiedHousingUnits: data[1].B25012_002E
       }
       res.json(countyResult)
     })
@@ -72,18 +73,18 @@ server.get("/ac5_state", (req, res) => {
   const params = req.query
 
   const url =
-    "https://api.census.gov/data/2016/acs/acs5?get=NAME,B00001_001E,B00002_001E,B20002_001E,B25012_002E&for=state:" + params.stateCode + "&key=37ff14711f40a5b4b3f6d39f773e687047ea6ce3"
+    "https://api.census.gov/data/2016/acs/acs5?get=B00001_001E,B00002_001E,B20002_001E,B25012_002E&for=state:" + params.stateCode + "&key=37ff14711f40a5b4b3f6d39f773e687047ea6ce3"
 
   axios
     .get(url)
     .then(data => {
       let stateResult = {
-        totalPop: data.B00001_001E,
-        totalHousingUnits: data.B00002_001E,
-        medianIncome: data.B20002_001E,
-        ownerOccupiedHousingUnits: data.B25012_002E
+        totalPop: data[1].B00001_001E,
+        totalHousingUnits: data[1].B00002_001E,
+        medianIncome: data[1].B20002_001E,
+        ownerOccupiedHousingUnits: data[1].B25012_002E
       }
-      res.json(countyResult)
+      res.json(stateResult)
     })
     .catch(err => {
       res.json({ error: err })
@@ -92,19 +93,13 @@ server.get("/ac5_state", (req, res) => {
 
 server.get("/ac5_national", (req, res) => {
   const url =
-    "https://api.census.gov/data/2016/acs/acs5?get=NAME,B00001_001E,B00002_001E,B20002_001E,B25012_002E&for=us&key=37ff14711f40a5b4b3f6d39f773e687047ea6ce3"
+    "https://api.census.gov/data/2016/acs/acs5?get=NAME,B01001_001E,B01001_002E,B01001_026E,B25001_001E,B20002_001E,B25003_002E&for=us&key=37ff14711f40a5b4b3f6d39f773e687047ea6ce3"
 
   axios
     .get(url)
     .then(apiRes => {
-      let nationalResult = {
-        totalPop: apiRes.data.B00001_001E,
-        totalHousingUnits: apiRes.data.B00002_001E,
-        medianIncome: apiRes.data.B20002_001E,
-        ownerOccupiedHousingUnits: apiRes.data.B25012_002E
-      }
-      console.log(nationalResult)
-      res.json(nationalResult)
+      const results = censusResultToObj(apiRes.data)    
+      res.json(results)
     })
     .catch(err => {
       res.json({ error: err })
@@ -112,6 +107,55 @@ server.get("/ac5_national", (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000
+
 server.listen(PORT, () => {
   console.log(`Server http://localhost:${PORT}`)
 })
+
+function censusResultToObj(data) {
+  // Loop through results, using [0] as header for [1]
+  let obj = {}
+  for(let i=0; i < data[0].length;i++){
+    let header = data[0][i]
+    let val = data[1][i]
+    header = censusHeaderLookup(header);
+    obj[`${header}`] = val
+  }
+
+  //Get homeowner percentage
+  obj.homeownerPercentage = (obj.ownerOccupiedHousingUnits / obj.totalHousingUnits * 100).toFixed(2) + '%'
+  //Get male / female percentage
+  obj.MalePercentage = (obj.totalMalePopulation / obj.totalPopulation * 100).toFixed(2) + '%'
+  obj.FemalePercentage = (obj.totalFemalePopulation / obj.totalPopulation * 100).toFixed(2) + '%'
+
+  //return resulting obj
+  return obj
+}
+
+function censusHeaderLookup(hdr) {
+  switch(hdr) {
+    case 'NAME':
+      return 'Scope'
+      break
+    case 'B01001_001E':
+      return 'totalPopulation'
+      break
+    case 'B01001_002E':
+      return 'totalMalePopulation'
+      break
+    case 'B01001_026E':
+      return 'totalFemalePopulation'
+      break
+    case 'B25001_001E':
+      return 'totalHousingUnits'
+      break
+    case 'B20002_001E':
+      return 'medianIncome'
+      break
+    case 'B25003_002E':
+      return 'ownerOccupiedHousingUnits'
+      break
+    default:
+      return hdr
+  }
+}
